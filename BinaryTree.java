f87089ff8ef309fafcfe3884756f6ce69168231f
Seth Wiley <seth.wiley@temple.edu>
2021-04-02 12:43:55 -0400

    New Test

--- BinaryTree.java
+++ BinaryTree.java
@@ -3,5 +3,6 @@ import jdk.dynalink.beans.StaticClass;
 public class BinaryTree {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("New Test");
     }
 }
